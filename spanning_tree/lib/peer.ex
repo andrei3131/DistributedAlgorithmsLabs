653f59b01448b8b79f703677dcc325d7c4483bb1
ab7515 <andrei-octavian.brabete15@imperial.ac.uk>
2018-01-30 15:57:39 +0000

    Spanning tree done.

--- spanning_tree/lib/peer.ex
+++ spanning_tree/lib/peer.ex
@@ -3,25 +3,32 @@ defmodule Peer do
 def start() do
     IO.puts ["        Peer at ", DNS.my_ip_addr()]
     receive do
-    { :hello, neighbours} ->
-      IO.puts ["Peer ", inspect(self()), " forwards first :hello to neighbours ", inspect(neighbours)]
-      Enum.map(neighbours, fn(n) ->
-               send n, {:hello, neighbours, :parent, self()} end)
-      receiveMembership(neighbours, 1)
+         {:neighbours, neighbour_ids} ->
+            IO.puts ["Registering neighbours for node ", inspect(self()), ": ", inspect(neighbour_ids)]
+            registerNeighbours(neighbour_ids)
     end
 end
 
-defp receiveMembership(neighbours, countHello) do
+defp registerNeighbours(neighbours) do
+    receive do
+        {:hello, parent} ->
+            IO.puts ["Node ", inspect(self()), " has parent = ", inspect(parent)]
+            Enum.map(neighbours, fn(n) ->
+                                 send n, {:hello, self()} end)
+            receiveMembership(parent, neighbours, 1)
+    end
+end
+
+defp receiveMembership(parent, neighbours, countHello) do
   receive do
-      {:hello, _, :parent, parent_id} ->
-          Process.sleep(DAC.random(3) * 1000)
-          IO.puts ["Peer ", inspect(self()), " Parent ", inspect(parent_id), " Messages seen = ", inspect(countHello)]
-          receiveMembership(neighbours, countHello + 1)
+      {:hello, _} ->
+         IO.puts ["Peer ", inspect(self()), " Parent ", inspect(parent), " Messages seen = ", inspect(countHello + 1)]
+         receiveMembership(parent, neighbours, countHello + 1)
   after
       1000 -> IO.puts ["Peer ", inspect(self()), " Messages seen = ", inspect(countHello)]
   end
   Process.sleep(DAC.random(3) * 1000)
-  receiveMembership(neighbours, countHello + 1)
+  receiveMembership(parent, neighbours, countHello)
 end
 
 end  #module ------------
